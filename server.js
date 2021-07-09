a5708f4c1944085104ce1f4a781aaa765d5f5c82
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-07-09 20:16:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -37,24 +37,24 @@ const HttpErrors  = require('http-errors');
    app.use('*',cors());
 
 // to protect xxs attack
-   app.use(helmet({
-      contentSecurityPolicy: {
-          directives: {
-              defaultSrc: ["'self'"],
-              scriptSrc: ["'self'", "https://maps.googleapis.com", "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
-              "https://fonts.googleapis.com/icon?family=Material+Icons",
-              "https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js"
-              ,"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-              "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.csshttps://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css",
-              ,"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"],
-              connectSrc: ["'self'", "https://some-domain.com", "https://some.other.domain.com"],
-              styleSrc: ["'self'","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.csshttps://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", "fonts.googleapis.com", "'unsafe-inline'"],
-              fontSrc: ["'self'", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css","fonts.gstatic.com"],
-              imgSrc: ["'self'", "https://maps.gstatic.com", "https://maps.googleapis.com", "data:", "https://another-domain.com","https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js"],
-              frameSrc: ["'self'", "https://www.google.com"]
-          }
-      },
-  }));
+//    app.use(helmet({
+//       contentSecurityPolicy: {
+//           directives: {
+//               defaultSrc: ["'self'"],
+//               scriptSrc: ["'self'", "https://maps.googleapis.com", "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+//               "https://fonts.googleapis.com/icon?family=Material+Icons",
+//               "https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js"
+//               ,"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+//               "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.csshttps://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css",
+//               ,"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"],
+//               connectSrc: ["'self'", "https://some-domain.com", "https://some.other.domain.com"],
+//               styleSrc: ["'self'","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.csshttps://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", "fonts.googleapis.com", "'unsafe-inline'"],
+//               fontSrc: ["'self'", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css","fonts.gstatic.com"],
+//               imgSrc: ["'self'", "https://maps.gstatic.com", "https://maps.googleapis.com", "data:", "https://another-domain.com","https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js"],
+//               frameSrc: ["'self'", "https://www.google.com"]
+//           }
+//       },
+//   }));
 
 // for logging each request 
    app.use(morgan('dev'));
@@ -72,7 +72,12 @@ const HttpErrors  = require('http-errors');
 
 // intialize view and template engine
    app.use(express.static(__dirname + "/public/"));
-  
+   app.use('/w3/', express.static(__dirname + '/node_modules/w3-css/3/'));
+   app.use('/swiper/', express.static(__dirname + '/node_modules/swiper/'));
+
+
+
+
    app.set('views', 'views');
 
    app.set('view engine', 'ejs'); 
